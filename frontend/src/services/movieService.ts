81e6c4474a56e045d2791dfae3b7994707765633
kostas <konstantinidis.kon@yahoo.com>
2026-08-17 17:37:46 +0300

    add movieservice

--- frontend/src/services/movieService.ts
+++ frontend/src/services/movieService.ts
@@ -1,3 +1,11 @@
 import { Movie } from "../types/movie";
 
 const API_URL = 'http://localhost:5106/api/v1/Movie'
+
+export const getMovies = async(): Promise<Movie[]> => {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+        throw new Error('Error fetching movies');
+    }
+    return response.json();
+};
